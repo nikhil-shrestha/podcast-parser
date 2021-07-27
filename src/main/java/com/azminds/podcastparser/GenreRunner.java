7b14d9fb3abe7ad0ad3d69ebaf0ad8088694cf16
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-07-27 19:56:58 +0545

    added cmd runner and updated thread implenentation

---
+++ src/main/java/com/azminds/podcastparser/GenreRunner.java
@@ -0,0 +1,61 @@
+package com.azminds.podcastparser;
+
+import com.azminds.podcastparser.dao.entity.Genre;
+import com.azminds.podcastparser.dao.repository.GenreRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+@Component
+@Order(value = 1)
+public class GenreRunner implements CommandLineRunner {
+
+  private static final String API_ENDPOINT = "https://3rugykz490.execute-api.us-west-1.amazonaws.com/dev/apple/podcast/genre";
+
+  @Autowired
+  private GenreRepository genreRepository;
+
+  @Override
+  public void run(String... args) throws IOException {
+    List<Genre> genres = genreRepository.findAll();
+
+    if (genres.size() == 0) {
+      URL urlForGetRequest = new URL(API_ENDPOINT);
+      String readLine = null;
+      HttpURLConnection conn = (HttpURLConnection) urlForGetRequest.openConnection();
+      conn.setRequestMethod("GET");
+      int responseCode = conn.getResponseCode();
+
+      if (responseCode == HttpURLConnection.HTTP_OK) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuffer response = new StringBuffer();
+        while ((readLine = in.readLine()) != null) {
+          response.append(readLine);
+        }
+        in.close();
+        // print result
+        String json = response.toString();
+        JSONArray jsonArr = new JSONArray(json);
+        jsonArr.forEach(item -> {
+          JSONObject obj = (JSONObject) item;
+          Genre genre = new Genre();
+          genre.setGenreIdOld(obj.get("id").toString());
+          genre.setName(obj.get("name").toString());
+          genreRepository.save(genre);
+        });
+      } else {
+        System.out.println("GET NOT WORKED");
+      }
+    }
+  }
+}
--- src/main/java/com/azminds/podcastparser/PodcastThread.java
+++ src/main/java/com/azminds/podcastparser/PodcastThread.java
@@ -1,132 +1,151 @@
 package com.azminds.podcastparser;
 
 import be.ceau.itunesapi.response.Result;
-import com.azminds.podcastparser.domain.Genre;
-import com.azminds.podcastparser.repository.GenreRepository;
-import com.azminds.podcastparser.repository.PodcastRepository;
+import com.azminds.podcastparser.dao.entity.Genre;
+import com.azminds.podcastparser.dao.repository.GenreRepository;
+import com.azminds.podcastparser.dao.repository.PodcastRepository;
 import com.icosillion.podengine.models.Episode;
+import com.icosillion.podengine.models.ITunesItemInfo;
 import com.icosillion.podengine.models.Podcast;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.URL;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-class GenreData {
-  String genreId;
-  String genreName;
-}
+public class PodcastThread implements Runnable {
 
-public class PodcastThread extends Thread {
-
+  private Thread worker;
   private Result rslt;
-
-  public PodcastThread(Result rslt) {
-    this.rslt = rslt;
-  }
+  private final AtomicBoolean running = new AtomicBoolean(false);
 
   @Autowired
   PodcastRepository podcastRepository;
-
   @Autowired
   GenreRepository genreRepository;
 
-  public void run(){
-    try {
+  PodcastThread(Result rslt) {
+    this.rslt = rslt;
+  }
+
+  public void start() {
+    worker = new Thread(this);
+    worker.start();
+  }
+
+  public void stop() {
+    running.set(false);
+  }
+
+  @Override
+  public void run() {
+    running.set(true);
+    final long start = System.currentTimeMillis();
+    while (running.get()) {
       System.out.println(
         "Thread " + Thread.currentThread().getId()
           + " is running");
+      process();
+    }
+    System.out.println(
+      "Thread " + Thread.currentThread().getId()
+        + " has stopped. Elapsed time: {" + ((System.currentTimeMillis() - start) / 1000) + " second}");
+  }
 
-      com.azminds.podcastparser.domain.Podcast podcastEntity = new com.azminds.podcastparser.domain.Podcast(
-        rslt.getCollectionId(),
-        rslt.getCollectionName(),
-        rslt.getDescription(),
-        rslt.getCollectionViewUrl(),
-        rslt.getArtistName(),
-        rslt.getArtistViewUrl(),
-        rslt.getWrapperType(),
-        rslt.getKind(),
-        rslt.getFeedUrl(),
-        rslt.getPreviewUrl(),
-        rslt.getArtworkUrl30(),
-        rslt.getArtworkUrl60(),
-        rslt.getArtworkUrl100(),
-        rslt.getArtworkUrl512(),
-        rslt.getArtworkUrl600(),
-        rslt.getReleaseDate(),
-        rslt.getTrackCount(),
-        rslt.getCountry(),
-        rslt.getCopyright(),
-        rslt.getShortDescription(),
-        rslt.getLongDescription()
-      );
-      GenreData[] genreData = new GenreData[rslt.getGenreIds().size()];
-      int i = 0;
-      for (String id : rslt.getGenreIds()) {
-        genreData[i] = new GenreData();
-        genreData[i].genreId = id;
-        i++;
-      }
+  public void process() {
+    try {
+      Optional<com.azminds.podcastparser.dao.entity.Podcast> podcastDbData = this.podcastRepository.findByCollectionId(rslt.getCollectionId());
+      if (!podcastDbData.isPresent()) {
+        com.azminds.podcastparser.dao.entity.Podcast podcastEntity = new com.azminds.podcastparser.dao.entity.Podcast();
+        podcastEntity.setCollectionId(rslt.getCollectionId());
+        podcastEntity.setCollectionName(rslt.getCollectionName());
+        podcastEntity.setDescription(rslt.getDescription());
+        podcastEntity.setCollectionViewUrl(rslt.getCollectionViewUrl());
+        podcastEntity.setArtistName(rslt.getArtistName());
+        podcastEntity.setArtistViewUrl(rslt.getArtistViewUrl());
+        podcastEntity.setWrapperType(rslt.getWrapperType());
+        podcastEntity.setKind(rslt.getKind());
+        podcastEntity.setFeedUrl(rslt.getFeedUrl());
+        podcastEntity.setPreviewUrl(rslt.getPreviewUrl());
+        podcastEntity.setArtworkUrl30(rslt.getArtworkUrl30());
+        podcastEntity.setArtworkUrl60(rslt.getArtworkUrl60());
+        podcastEntity.setArtworkUrl100(rslt.getArtworkUrl100());
+        podcastEntity.setArtworkUrl512(rslt.getArtworkUrl512());
+        podcastEntity.setArtworkUrl600(rslt.getArtworkUrl600());
+        podcastEntity.setReleaseDate(rslt.getReleaseDate());
+        podcastEntity.setTrackCount(rslt.getTrackCount());
+        podcastEntity.setCountry(rslt.getCountry());
+        podcastEntity.setCountry(rslt.getCountry());
+        podcastEntity.setCopyright(rslt.getCopyright());
+        podcastEntity.setShortDescription(rslt.getShortDescription());
+        podcastEntity.setLongDescription(rslt.getLongDescription());
 
-      i = 0;
-      for (String name : rslt.getGenres()) {
-        genreData[i].genreName = name;
-        i++;
-      }
-
-      for (GenreData genre : genreData) {
-        Optional<Genre> genreDbData = this.genreRepository.findByGenreIdOld(genre.genreId);
-        com.azminds.podcastparser.domain.Genre genreEntity;
-
-        if (!genreDbData.isPresent()) {
-          genreEntity = new com.azminds.podcastparser.domain.Genre(genre.genreName, genre.genreId);
-          this.genreRepository.save(genreEntity);
-
-          podcastEntity.addGenre(genreEntity);
-        } else {
-          genreEntity = genreDbData.get();
+        for (String id : rslt.getGenreIds()) {
+          if (!id.equals("26")){
+            Optional<Genre> genreDbData = this.genreRepository.findByGenreIdOld(id);
+            if (genreDbData.isPresent()){
+              Genre genreEntity = genreDbData.get();
+              podcastEntity.addGenre(genreEntity);
+            }
+          }
         }
-        podcastEntity.addGenre(genreEntity);
 
-        System.out.println("added Genre!!!!");
-      }
+        URL url = new URL(rslt.getFeedUrl());
+        try {
+          Podcast podcastData = new Podcast(url);
+          System.out.println("- " + podcastData.getTitle() + " " + podcastData.getEpisodes().size());
+          podcastEntity.setDescription(podcastData.getDescription());
+          podcastEntity.setEpisodeCount(podcastData.getEpisodes().size());
 
-      System.out.println("URL::" + rslt.getFeedUrl());
-      System.out.println("isValid URL>>>");
-      try {
-        Podcast podcastData = new Podcast(new URL(rslt.getFeedUrl()));
-        System.out.println("- " + podcastData.getTitle() + " " + podcastData.getEpisodes().size());
-        podcastEntity.setDescription(podcastData.getDescription());
-        podcastEntity.setEpisodeCount(podcastData.getEpisodes().size());
+          Collection<Episode> episodes = podcastData.getEpisodes();
+          // List all episodes
+          for (Episode episode : episodes) {
+            System.out.println("\n- " + episode.getGUID());
+            String description1 = "";
+            try { description1 = episode.getDescription(); } catch (Exception e) { }
 
-        System.out.println("get Episode Collection>>>");
-        Collection<Episode> episodes = podcastData.getEpisodes();
-        // List all episodes
-        for (Episode episode : episodes) {
-          com.azminds.podcastparser.domain.Episode episodeEntity = new com.azminds.podcastparser.domain.Episode(
-            episode.getTitle(),
-            episode.getDescription(),
-            episode.getGUID(),
-            episode.getLink(),
-            episode.getPubDate(),
-            episode.getITunesInfo().getDuration(),
-            episode.getEnclosure().getURL(),
-            episode.getEnclosure().getLength(),
-            episode.getEnclosure().getType()
-          );
-          podcastEntity.addEpisode(episodeEntity);
+            String hostedUrl = "";
+            try { hostedUrl = episode.getLink().toString(); } catch (Exception e) { }
+            String duration = "";
+            ITunesItemInfo iTunesItemInfo = episode.getITunesInfo();
+            try { duration = iTunesItemInfo.getDuration(); } catch (Exception e) { }
+
+            Episode.Enclosure epEnclousure = episode.getEnclosure();
+            String episodeLink = "";
+            try { episodeLink = epEnclousure.getURL().toString(); } catch (Exception e) { }
+            long episodeLength = 0;
+            try { episodeLength = epEnclousure.getLength(); } catch (Exception e) { }
+            String type = "";
+            try { type = epEnclousure.getType(); } catch (Exception e) { }
+
+            com.azminds.podcastparser.dao.entity.Episode episodeEntity = new com.azminds.podcastparser.dao.entity.Episode();
+            episodeEntity.setTitle(episode.getTitle());
+            episodeEntity.setDescription(description1);
+            episodeEntity.setGuid(episode.getGUID());
+            episodeEntity.setHostedUrl(hostedUrl);
+            episodeEntity.setPubDate(episode.getPubDate());
+            episodeEntity.setDurationString(duration);
+            episodeEntity.setLink(episodeLink);
+            episodeEntity.setDuration(episodeLength);
+            episodeEntity.setType(type);
+            podcastEntity.addEpisode(episodeEntity);
+          }
+
+        } catch (Exception e) {
+          System.out.println("[Episode] Exception::");
+          e.printStackTrace();
         }
-      } catch (Exception e) {
-        System.out.println("[Episode] Exception::");
-        e.printStackTrace();
+        System.out.println("before save!!!!");
+        this.podcastRepository.save(podcastEntity);
       }
 
-      System.out.println("before save!!!!");
-      this.podcastRepository.save(podcastEntity);
-      Thread.sleep(50);
+      System.out.println("Thread Terminated!!");
     } catch (Exception e) {
       // Throwing an exception
       System.out.println("Exception is caught");
+      e.printStackTrace();
     }
+    this.stop();
   }
 }
