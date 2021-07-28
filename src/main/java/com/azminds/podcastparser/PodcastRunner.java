910b17ad483201f0cf944fe745248af0e79a4695
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-07-28 15:50:12 +0545

    added threadpool config and introduced @Async

--- src/main/java/com/azminds/podcastparser/PodcastRunner.java
+++ src/main/java/com/azminds/podcastparser/PodcastRunner.java
@@ -1,39 +1,84 @@
 package com.azminds.podcastparser;
 
-import com.azminds.podcastparser.dao.repository.GenreRepository;
-import com.azminds.podcastparser.dao.repository.PodcastRepository;
+import com.azminds.podcastparser.service.PodcastService;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Profile("test")
 @Component
 @Order(value = 3)
 public class PodcastRunner implements CommandLineRunner {
 
+  private static final Logger logger = LoggerFactory.getLogger(PodcastRunner.class);
+
   @Autowired
-  private PodcastRepository podcastRepository;
-  @Autowired
-  private GenreRepository genreRepository;
+  private PodcastService service;
+
+  public List<PodcastCSV> readCsv(String SAMPLE_CSV_FILE_PATH) {
+    List<PodcastCSV> podcasts = new ArrayList<>();
+    try (
+      Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH))
+    ) {
+      CSVReader csvReader = new CSVReader(reader);
+
+      // Reading Records One by One in a String array
+      String[] nextRecord;
+      while ((nextRecord = csvReader.readNext()) != null) {
+        PodcastCSV podcast = new PodcastCSV();
+        podcast.setId(nextRecord[0]);
+        podcast.setUrl(nextRecord[1]);
+        podcast.setItunesId(nextRecord[2]);
+        podcast.setOriginalUrl(nextRecord[3]);
+        podcast.setNewestItemPubdate(nextRecord[4]);
+        podcast.setOldestItemPubdate(nextRecord[5]);
+        podcast.setLanguage(nextRecord[6]);
+        podcasts.add(podcast);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (CsvValidationException e) {
+      e.printStackTrace();
+    }
+    return podcasts;
+  }
 
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) {
     System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
-
-    ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-    threadPoolTaskExecutor.setCorePoolSize(100);
-    threadPoolTaskExecutor.setMaxPoolSize(100);
-    threadPoolTaskExecutor.setKeepAliveSeconds(3000);
-    threadPoolTaskExecutor.setQueueCapacity(500);
-    // The thread pool needs to be initialized!
-    threadPoolTaskExecutor.initialize();
-    // 100 threads
-    for (int i = 0; i < 10; i++) {
-      PodcastThread pdThread = new PodcastThread(podcastRepository, genreRepository, i + 1);
-      threadPoolTaskExecutor.submit(pdThread);
+    for (int i = 1; i <= 10; i++) {
+      List<PodcastCSV> records = readCsv("FileNumber_" + i + ".csv");
+      logger.debug("{}", records);
+      Partition<PodcastCSV> arrayChunk = Partition.ofSize(records, 100);
+      logger.debug("{}", arrayChunk);
+      arrayChunk.forEach(arr -> {
+        Date date = new Date();
+        long now = date.getTime();
+        logger.debug("Loop Start Time: " + now + Thread.currentThread().getName());
+        try {
+          Collection<String> arrIds = arr.stream().map(PodcastCSV::getItunesId).collect(Collectors.toList());
+          service.saveApplePodcast(arrIds);
+          logger.debug("Loop Time taken: " + ((new Date().getTime() - now) / 1000) + " second " + Thread.currentThread().getName());
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      });
     }
   }
 }
--- src/main/java/com/azminds/podcastparser/dao/repository/GenreRepository.java
+++ src/main/java/com/azminds/podcastparser/dao/repository/GenreRepository.java
@@ -3,13 +3,12 @@ package com.azminds.podcastparser.dao.repository;
 import com.azminds.podcastparser.dao.entity.GenreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface GenreRepository extends JpaRepository<GenreEntity, Long> {
 
-  @Query("SELECT s FROM Genre s WHERE s.genreIdOld = ?1")
-  Optional<GenreEntity> findByGenreIdOld(String id);
+  @Query("SELECT s FROM Genre s WHERE s.genreIdOld = :id")
+  GenreEntity findByGenreIdOld(@Param("id") String id);
 }
